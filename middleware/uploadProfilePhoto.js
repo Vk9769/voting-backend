737da5fb59d1c45ca7fe520a539827c2ca1e8464
Vk9769 <vaihhavkothare@gmail.com>
2025-12-19 16:10:51 +0530

    VOTER PROFILE fetch update  ADDED.

--- middleware/uploadProfilePhoto.js
+++ middleware/uploadProfilePhoto.js
@@ -8,7 +8,6 @@ export const uploadProfilePhoto = multer({
   storage: multerS3({
     s3,
     bucket: BUCKET,
-    acl: "public-read",
     contentType: multerS3.AUTO_CONTENT_TYPE,
     key: (req, file, cb) => {
       const userId = req.user.userId;
