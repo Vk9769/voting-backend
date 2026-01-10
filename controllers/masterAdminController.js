b17fa925961c783ed9f8a3fbc1bad87a19136dd2
Vk9769 <vaihhavkothare@gmail.com>
2026-01-10 18:15:23 +0530

    CREATE AGENT.

--- controllers/masterAdminController.js
+++ controllers/masterAdminController.js
@@ -152,7 +152,7 @@ export const getAllBoothsHierarchy = async (req, res) => {
         district,
         ac_name_no   AS assembly_constituency,
         part_name_no AS part_name,
-        MIN(name)      AS name
+        MIN(name)      AS name,
         COUNT(*)     AS booths
       FROM booths
       GROUP BY state, district, ac_name_no, part_name_no
