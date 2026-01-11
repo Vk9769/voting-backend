0b211c736de7dca9a5b539b74e15ba772f0a09ac
Vk9769 <vaihhavkothare@gmail.com>
2026-01-11 14:06:32 +0530

    CREATE AGENT ALLOCATE BOOTH.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import bodyParser from "body-parser";
 
 import masterAdminRoutes from "./routes/masteradmin.js";
 import authRoutes from "./routes/auth.js";
@@ -14,7 +13,6 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 // HEALTH CHECK (ALB)
 app.get("/health", (req, res) => {
