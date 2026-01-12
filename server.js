c005db62abaf8463427e8d91bc3d3e4a1feee14a
Vk9769 <vaihhavkothare@gmail.com>
2026-01-12 15:31:03 +0530

    CREATE AGENT PHOTO UPLOAD FIX.

--- server.js
+++ server.js
@@ -11,7 +11,19 @@ import notificationRoutes from "./routes/notification.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: "*", // allow all for now
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Accept"
+  ],
+  credentials: false
+}));
+app.options("*", cors());
+
 app.use(express.json());
 
 // HEALTH CHECK (ALB)
