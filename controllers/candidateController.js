7ad94047b80a8e68b6edeef3745f73c3c19f85e0
Vk9769 <vaihhavkothare@gmail.com>
2026-02-11 15:16:17 +0530

    ADD CANDIDATE ADDED.

--- controllers/candidateController.js
+++ controllers/candidateController.js
@@ -1,5 +1,5 @@
 import { pool } from "../services/db.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 
 /* =====================================================
    CREATE CANDIDATE (UPGRADED)
